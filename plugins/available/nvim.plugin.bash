7d1ed484e8e86f1dd9da4210e545f8dba437c959
ppdx999 <ppdx999@gmail.com>
2023-10-07 17:29:14 +0900

    add alias to apply nvim/init.lua

--- plugins/available/nvim.plugin.bash
+++ plugins/available/nvim.plugin.bash
@@ -7,5 +7,5 @@ mkdir -p ~/.config/nvim
 cp -r "${NVIM_DIR}/." ~/.config/nvim
 
 alias nv='nvim'
+alias nvim-apply-conf='cp -r ${NVIM_DIR}/. ~/.config/nvim'
 alias edit-nvim-conf='${EDITOR:-vim} ${NVIM_DIR}/init.lua'
-
