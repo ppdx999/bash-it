a5949a8356047958a675447642068f4e0edc13d2
ppdx999 <ppdx999@gmail.com>
2023-10-07 17:26:56 +0900

    fixup bugs of dobuling ~/.tmux.conf everytime load ~/.bashrc

--- plugins/available/tmux.plugin.bash
+++ plugins/available/tmux.plugin.bash
@@ -4,7 +4,7 @@ cite about-plugin
 about-plugin 'setup tmux'
 
 TMUX_CONF_PATH="${BASH_IT}/vendor/github.com/ppdx999/tmux/.tmux.conf"
-cat "${TMUX_CONF_PATH}" >> ~/.tmux.conf
+cp "${TMUX_CONF_PATH}" ~/.tmux.conf
 
 alias tmux="TERM=xterm-256color tmux"
 alias edit-tmux-conf='${EDITOR:-vim} ${TMUX_CONF_PATH}'
