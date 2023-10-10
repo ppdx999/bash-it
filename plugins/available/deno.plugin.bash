0a525d6f558ff95f885253995d1fa6cf04c56446
ppdx999 <ppdx999@gmail.com>
2023-10-10 11:02:02 +0900

    [deno] fixup deno's init prcess rquire deno problem

--- plugins/available/deno.plugin.bash
+++ plugins/available/deno.plugin.bash
@@ -1,8 +1,6 @@
 cite about-plugin
 about-plugin 'setup deno'
 
-_command_exists deno || return
-
 if [[ -d "$HOME/.deno" ]]; then
   export DENO_INSTALL="$HOME/.deno"
   export PATH="$DENO_INSTALL/bin:$PATH"
